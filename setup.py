708fb56f9d7a4fabdd2656724bb203fcd49281b0
Lovmong <1967471283@qq.com>
2024-01-16 18:23:48 +0800

    update version

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup, find_packages
 # To use a consistent encoding
 from codecs import open
 from os import path
+import sys
+
+sys.path.append('./sunfounder_controller')
+from version import __version__
 
 here = path.abspath(path.dirname(__file__))
 
@@ -10,14 +14,13 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
-
 setup(
     name='sunfounder_controller',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="0.0.1",
+    version=__version__,
 
     description='Picrawler gait Library for Raspberry Pi',
     long_description=long_description,
